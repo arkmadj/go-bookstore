9f141438a46f3fe24e76c0658c7a9b2516d2e598
arkmadj <ahmadjinadu@gmail.com>
2022-09-01 14:05:51 +0100

    Fixed db connection

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "ahmad:qwerty123&Q/simplerest?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "root:qwerty123&Q@/go_book_store?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
